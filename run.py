7cd3be8a074ca5062b8a655c9011543fd6c2ba6c
Chi Gabriel <chigabriel3025@gmail.com>
2025-04-25 02:22:46 -0700

    Setting debug to true

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
-# File: run.py
 
 from kapricorn import create_app
 import os
 
-# Load environment variables from .env file if it exists
-# This is helpful if run.py is executed directly without activating env first in some scenarios
 from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
@@ -12,5 +9,4 @@ load_dotenv(os.path.join(basedir, '.env'))
 app = create_app()
 
 if __name__ == '__main__':
-    # Set debug=True for development, but False for production!
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
